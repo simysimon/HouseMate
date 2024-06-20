9a18169baf91a504fef4afba8e4f3bf71d836477
simysimon <simysimysimon21@gmail.com>
2024-06-20 17:27:05 -0400

    Initial Commit

---
+++ HouseMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HouseMate
+//
+//  Created by Simon Nguyen on 6/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HouseMate/HouseMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HouseMateApp.swift
+//  HouseMate
+//
+//  Created by Simon Nguyen on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct HouseMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
